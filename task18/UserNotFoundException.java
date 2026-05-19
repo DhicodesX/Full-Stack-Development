0f5dc77aad06da9b58e1240fe5ee9f80f9e2950f
DHIVYA SV <vtu26230@veltech.edu.in>
2026-05-19 20:00:08 +0530

    Delete task18 directory

--- task18/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.usermanagement.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
